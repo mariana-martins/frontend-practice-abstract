a71b772c895567bcd3494e7b566e5d887a8a4e1e
mariana-martins <marianamartinsmenezes@gmail.com>
2025-09-02 11:37:05 +1200

    Update project configuration and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+
+import { useState } from 'react';
+
 import './App.css';
+import reactLogo from './assets/react.svg';
 import theme from './theme';
+import viteLogo from '/vite.svg';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -20,19 +22,13 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <button style={{ backgroundColor: theme.colors.brand.primary }}>
-        Click me
-      </button>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <button style={{ backgroundColor: theme.colors.brand.primary }}>Click me</button>
     </ThemeProvider>
   );
 }
